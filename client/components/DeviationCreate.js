3abfc033fe8c24a3a7393680b14ea4f2f2663161
Michael <mpalermo@cognatebioservices.com>
2018-10-15 10:47:33 -0500

    changes to header for logout

--- client/components/DeviationCreate.js
+++ client/components/DeviationCreate.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import { Link, hashHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import fetchDeviationsQuery from '../queries/fetchDeviations';
 import addDeviation from '../queries/addDeviation';
 
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import AuthForm from './AuthForm';
-import { graphql } from 'react-apollo';
 import mutation from '../queries/User/Login';
+import { graphql } from 'react-apollo';
 import query from '../queries/User/fetchCurrentUser';
 import { hashHistory } from 'react-router';
 
 class LoginForm extends Component {
     constructor(props) {
         super(props);
+
         this.state = { errors: [] };
     }
-    // componentDidMount(){
-    //     _isMounted: true;
-    // }
+
     componentWillUpdate(nextProps) {
         // this.props // the old, current set of props
         // nextProps // the next set of props that will be in place
         // when the component rerenders
-        if(!this.props.data.user && nextProps.data.user) {
-            // redirect to dashboard
+        if (!this.props.data.user && nextProps.data.user) {
+            // redirect to dashboard!!!!
             hashHistory.push('/dashboard');
         }
     }
 
     onSubmit({ name, password }) {
-        // debugger;
         this.props.mutate({
             variables: { name, password },
-            refetchqueries: [{ query }],
-            // awaitRefetchQueries: true
+            refetchQueries: [{ query }]
         }).catch(res => {
             const errors = res.graphQLErrors.map(error => error.message);
             this.setState({ errors });
-            // this.refs.test_ref ? this.setState({ errors }) : null;
-        });    
+        });
     }
 
     render() {
@@ -41,7 +37,6 @@ class LoginForm extends Component {
             <div>
                 <h3>Login</h3>
                 <AuthForm
-                    // ref="test_ref"
                     errors={this.state.errors}
                     onSubmit={this.onSubmit.bind(this)}
                 />
@@ -51,5 +46,5 @@ class LoginForm extends Component {
 }
 
 export default graphql(query)(
-    graphql(mutation)(LoginForm))
-;
+    graphql(mutation)(LoginForm)
+);
